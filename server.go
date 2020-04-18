7296d8aefade8a6e9479861a4aaccf8de26fa07d
abda <abda@warungpintar.co>
2020-04-18 17:00:11 +0700

    first

--- server.go
+++ server.go
@@ -1,45 +1,23 @@
+// export PORT=5000
+// demoapp
+
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 )
 
 func main() {
-	http.HandleFunc("/", halo)
-	fmt.Println("Server started on port " + os.Getenv("PORT"))
+	http.HandleFunc("/", hello)
+	fmt.Println("listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		panic(err)
 	}
-
 }
 
-func halo(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	keys := r.URL.Query()["key"]
-	msg := "dunia"
-	if len(keys) > 0 {
-		msg = (keys[0])
-	}
-
-	payload := struct {
-		Status  string `json:"status"`
-		Message string `json:"message"`
-	}{
-
-		Status:  "Success",
-		Message: "Hello " + msg,
-	}
-
-	response, err := json.Marshal(payload)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(response)
+func hello(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "Hello World by Chee Yeo 2015")
 }
